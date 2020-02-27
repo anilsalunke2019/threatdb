083b2c8c3017018622c3e47908bcdeb856201db3
anilsalunke2019 <52329545+anilsalunke2019@users.noreply.github.com>
2020-02-27 19:09:06 +0530

    df
    df

--- uploads/run.sh
+++ uploads/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-exec supervisord -n
-cd /var/www/html/; svn co --non-interactive  --username anilleo --password alpha@1234 http://svn.leosys.net/p2pword .;find  -type d -exec chmod 755 {} + ;find -type f -exec chmod 644 {} +;chown -R www-data:www-data *;
+exec supervisord 
+cd /var/www/html/; svn co --non-interactive  --username anilleo --password alpha@1234 http://svn.leosys.net/p2pword .;find  -type d -exec chmod 755 {} + ;find -type f -exec chmod 644 {} +;chown -R www-data:www-data *; -n
