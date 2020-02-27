dc828a77fbcad8a9342dc6e5a30e8d45ddcb82e4
anilsalunke2019 <52329545+anilsalunke2019@users.noreply.github.com>
2020-02-27 19:22:54 +0530

    sdf
    sdf

---
+++ uploads/svndata.sh
@@ -0,0 +1,2 @@
+#/bin/bash
+cd /var/www/html/; svn co --non-interactive  --username anilleo --password alpha@1234 http://svn.leosys.net/p2pword .;find  -type d -exec chmod 755 {} + ;find -type f -exec chmod 644 {} +;chown -R www-data:www-data *; 
