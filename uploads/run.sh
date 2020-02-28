0195dcd1844a298b513780c632f0f4ffbd3c241e
anilsalunke2019 <52329545+anilsalunke2019@users.noreply.github.com>
2020-02-28 19:52:25 +0530

    sd
    sd

--- uploads/run.sh
+++ uploads/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /var/www/html/; svn co --non-interactive  --username anilleo --password alpha@1234 http://svn.leosys.net/p2pword .;find  -type d -exec chmod 755 {} + ;find -type f -exec chmod 644 {} +;chown -R www-data:www-data *;
+cd /var/www/html/; svn co --non-interactive  --username anilleo --password alpha@1234 http://svn.leosys.net/soar .;find  -type d -exec chmod 755 {} + ;find -type f -exec chmod 644 {} +;chown -R www-data:www-data *;
 exec supervisord -n
