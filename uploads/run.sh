ed39764d4067d24492130c72f39570dacd20989c
anilsalunke2019 <52329545+anilsalunke2019@users.noreply.github.com>
2020-03-18 18:52:01 +0530

    dsf
    sd

--- uploads/run.sh
+++ uploads/run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 cd /var/www/html/;exec /usr/bin/git clone https://github.com/appyens/threat-intelligent-hub.git .;find -type d -exec chmod 755 {} + ;find  -type f -exec chmod 644 {} +;chown -R www-data:www-data *;exec /usr/bin/python3 -m pip install -r /requirements.txt;exec /usr/bin/python3 /var/www/html/manage.py;
+exec supervisord -n
