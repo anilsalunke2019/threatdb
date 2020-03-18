3ee4a21f4328c1f535b9c1c860bf04dbd426b46b
anilsalunke2019 <52329545+anilsalunke2019@users.noreply.github.com>
2020-03-18 20:02:14 +0530

    sdf
    sdf

--- uploads/run.sh
+++ uploads/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-find /var/www/html/threat/ -type d -exec chmod 755 {} + ;find /var/www/html/threat/ -type f -exec chmod 644 {} +;chown -R www-data:www-data *;exec /usr/bin/python3 -m pip install -r /requirements.txt;exec /usr/bin/python3 /var/www/html/threat/manage.py;
+exec /usr/bin/python3 -m pip install -r /requirements.txt;exec /usr/bin/python3 /var/www/html/threat/manage.py;
 exec supervisord -n
