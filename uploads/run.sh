3e19a7c57ecfdee72b46b6aadd1efa70ab1613ff
anilsalunke2019 <52329545+anilsalunke2019@users.noreply.github.com>
2020-03-18 17:56:26 +0530

    sdf
    sdf

--- uploads/run.sh
+++ uploads/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-exec cd /var/www/html/; git clone https://github.com/appyens/threat-intelligent-hub.git .;find  -type d -exec chmod 755 {} + ;find -type f -exec chmod 644 {} +;chown -R www-data:www-data *;
+cd /var/www/html/;
+exec git clone https://github.com/appyens/threat-intelligent-hub.git .;find  -type d -exec chmod 755 {} + ;find -type f -exec chmod 644 {} +;chown -R www-data:www-data *;
 exec python3 -m pip install -r /requirements.txt;
 exec python3 /var/www/html/manage.py;
 
