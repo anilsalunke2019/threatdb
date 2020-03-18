19a0112c89c6fca1db501467aede06dc74175cea
anilsalunke2019 <52329545+anilsalunke2019@users.noreply.github.com>
2020-03-18 17:07:03 +0530

    pythonrun
    pythonrun

--- uploads/run.sh
+++ uploads/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd /var/www/html/; git clone https://github.com/appyens/threat-intelligent-hub.git .;find  -type d -exec chmod 755 {} + ;find -type f -exec chmod 644 {} +;chown -R www-data:www-data *;
-exec python3 -m pip install -r /requirements.txt 
+exec python3 -m pip install -r /requirements.txt
+python3 /var/www/html/manage.py
+
 exit 0
