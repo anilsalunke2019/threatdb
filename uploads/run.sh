c53c6866e00618e0c9dd32de290e00a10eccf5e0
anilsalunke2019 <52329545+anilsalunke2019@users.noreply.github.com>
2020-03-18 16:01:59 +0530

    sdf
    sdf

--- uploads/run.sh
+++ uploads/run.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 git clone https://github.com/appyens/threat-intelligent-hub /var/www/html/threat-intelligent-hub/
+pip install -r /requirements.txt
+python3 /var/www/html/threat-intelligent-hub/manage.py
